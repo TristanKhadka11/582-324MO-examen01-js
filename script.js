940415867afaaf67391f5239fba879f8ac20f420
TristanKhadka11 <evoeleven.contact@gmail.com>
2023-10-27 15:50:21 -0400

    v2

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Veuillez écrire votre JavaScript ici
+
+const image1 = document.querySelectorAll('img')
+image1.src = ``;
+
+
+
+
+
+
+
+function afficherBouton() {
+    const bouton = document.getElementById("bouton");
+    bouton.className = 'afficher'
+
+  
+  }
+  
+  const afficher = document.getElementById('choix')
+  afficher.addEventListener("click", afficherBouton);
+
+
+
+  function validerChoix() {
+    const bouton2 = document.getElementById("bouton");
+    bouton2.className = 'hidden'
+
+
+  }
+  
+  const bouton1 = document.getElementById('bouton')
+  bouton1.addEventListener("click", validerChoix);
+
+
+
+
